fa1e4a8c29d3902b7b81dbf1b977068207865f93
Frankie Valentine <frankievalentine9@gmail.com>
2020-12-30 18:37:35 -0800

    refactor misc, firestore, and chakra setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,42 @@
-import { AuthProvider } from '../lib/auth';
-import '../styles/globals.css';
+import Head from 'next/head';
+import { Global, css } from '@emotion/core';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { AuthProvider } from '@/lib/auth';
+
+import customTheme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,23 @@
-import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { Heading, Button, Text, Code } from '@chakra-ui/core';
+
+import { useAuth } from '@/lib/auth';
 
 const Home = () => {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>R2025 & Fast Feedback</h1>
-        <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
-        <div>{auth?.user?.email}</div>
-        {auth?.user && (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+    <div>
+      <main>
+        <Heading>R2025 & Fast Feedback</Heading>
+        <Text>
+          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+        </Text>
+        {auth.user ? (
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 };
